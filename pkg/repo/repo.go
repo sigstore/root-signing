90d697926e7a075017a6fb94469a580ab24fc6a0
Asra Ali <asraa@google.com>
2022-04-22 13:40:16 -0500

    enhance verification script
    Signed-off-by: Asra Ali <asraa@google.com>

--- pkg/repo/repo.go
+++ pkg/repo/repo.go
@@ -3,32 +3,79 @@ package repo
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/theupdateframework/go-tuf"
 	"github.com/theupdateframework/go-tuf/data"
 	"github.com/theupdateframework/go-tuf/verify"
 )
 
-func CreateDb(store tuf.LocalStore) (*verify.DB, error) {
-	db := verify.NewDB()
+func CreateDb(store tuf.LocalStore) (db *verify.DB, thresholds map[string]int, err error) {
+	db = verify.NewDB()
 	root, err := GetRootFromStore(store)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	for id, k := range root.Keys {
 		if err := db.AddKey(id, k); err != nil {
 			// ignore ErrWrongID errors by TAP-12
 			if _, ok := err.(verify.ErrWrongID); !ok {
-				return nil, err
+				return nil, nil, err
 			}
 		}
 	}
+	roles := make(map[string]bool, len(root.Roles))
+	thresholds = make(map[string]int, len(root.Roles))
 	for name, role := range root.Roles {
 		if err := db.AddRole(name, role); err != nil {
-			return nil, err
+			return nil, nil, err
+		}
+		thresholds[name] = role.Threshold
+		roles[name] = true
+	}
+
+	// now add delegations
+	meta, err := store.GetMeta()
+	if err != nil {
+		return nil, nil, err
+	}
+	for metaName, metadata := range meta {
+		name := strings.TrimSuffix(metaName, ".json")
+		if _, ok := roles[name]; ok && !strings.EqualFold(name, "targets") {
+			continue
+		}
+		s := &data.Signed{}
+		if err := json.Unmarshal(metadata, s); err != nil {
+			return nil, nil, fmt.Errorf("error unmarshalling for targets %q: %w", metaName, err)
+		}
+		t := &data.Targets{}
+		if err := json.Unmarshal(s.Signed, t); err != nil {
+			return nil, nil, fmt.Errorf("error unmarshalling signed data for targets %q: %w", metaName, err)
+		}
+
+		if t.Delegations == nil {
+			continue
+		}
+
+		for id, k := range t.Delegations.Keys {
+			if err := db.AddKey(id, k); err != nil {
+				// ignore ErrWrongID errors by TAP-12
+				if _, ok := err.(verify.ErrWrongID); !ok {
+					return nil, nil, err
+				}
+			}
+		}
+		for _, drole := range t.Delegations.Roles {
+			role := &data.Role{Threshold: drole.Threshold, KeyIDs: drole.KeyIDs}
+			thresholds[drole.Name] = drole.Threshold
+			if err := db.AddRole(drole.Name, role); err != nil {
+				return nil, nil, err
+			}
 		}
 	}
-	return db, nil
+
+	return db, thresholds, nil
 }
 
 func GetRootFromStore(store tuf.LocalStore) (*data.Root, error) {
@@ -89,4 +136,15 @@ func GetMetaFromStore(msg []byte, name string) (interface{}, error) {
 		return nil, err
 	}
 	return meta, nil
+}
+
+func IsVersionedManifest(name string) bool {
+	parts := strings.Split(name, ".")
+	// Versioned manifests have the form "x.role.json"
+	if len(parts) < 3 {
+		return false
+	}
+
+	_, err := strconv.Atoi(parts[0])
+	return err == nil
 }

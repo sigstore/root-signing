633c699a33237ead52ee37c5874a8c2788c001ea
Asra Ali <asraa@google.com>
2022-04-27 11:11:14 -0500

    Add configuration to specify custom target metadata and fix revocation bug
    Signed-off-by: Asra Ali <asraa@google.com>

--- scripts/step-1.5.sh
+++ scripts/step-1.5.sh
@@ -7,6 +7,7 @@ if [ -z "$GITHUB_USER" ]; then
     echo "Set GITHUB_USER"
     exit
 fi
+# Online top-level keys
 if [ -z "$TIMESTAMP_KEY" ]; then
     echo "Set TIMESTAMP_KEY"
     exit
@@ -15,6 +16,7 @@ if [ -z "$SNAPSHOT_KEY" ]; then
     echo "Set SNAPSHOT_KEY"
     exit
 fi
+# Delegation keys
 if [ -z "$REKOR_KEY" ]; then
     echo "Set REKOR_KEY"
     exit
@@ -23,6 +25,11 @@ if [ -z "$STAGING_KEY" ]; then
     echo "Set STAGING_KEY"
     exit
 fi
+if [ -z "$REVOCATION_KEY" ]; then
+    echo "Set REVOCATION_KEY"
+    exit
+fi
+# Repo options
 if [ -z "$PREV_REPO" ]; then
     echo "Set PREV_REPO"
     exit
@@ -31,9 +38,6 @@ if [ -z "$CEREMONY_DATE" ]; then
     CEREMONY_DATE=$(date '+%Y-%m-%d')
 fi
 export REPO=$(pwd)/ceremony/$CEREMONY_DATE
-
-# Copy the previous keys and repository into the new repository.
-cp -r ${PREV_REPO}/* ${REPO}
 
 # Dump the git state
 git status
@@ -44,13 +48,19 @@ git checkout main
 git pull upstream main
 git status
 
+# Copy the previous keys and repository into the new repository.
+cp -r ${PREV_REPO}/* ${REPO}
+mkdir -p ${REPO}/staged/targets
+
 # Setup the root and targets
-./tuf init -repository $REPO -target targets/fulcio.crt.pem -target targets/fulcio_v1.crt.pem -target targets/rekor.pub -target targets/ctfe.pub -target targets/artifact.pub -snapshot ${SNAPSHOT_KEY} -timestamp ${TIMESTAMP_KEY} -previous ${PREV_REPO}
+./tuf init -repository $REPO -target-meta config/targets-metadata.yml -snapshot ${SNAPSHOT_KEY} -timestamp ${TIMESTAMP_KEY} -previous "${PREV_REPO}"
 # Add rekor delegation
 cp targets/rekor.pub targets/rekor.0.pub
-./tuf add-delegation -repository $REPO -name "rekor" -key $REKOR_KEY -path "rekor.*.pub" -target targets/rekor.0.pub
+./tuf add-delegation -repository $REPO -name "rekor" -key $REKOR_KEY -path "rekor.*.pub" -target-meta config/rekor-metadata.yml
 # Add staging project delegation
 ./tuf add-delegation -repository $REPO -name "staging" -key $STAGING_KEY -path "*"
+# TODO: Add revoked project delegation
+./tuf add-delegation -repository $REPO -name "revocation" -key $REVOCATION_KEY -path "*" -target-meta config/revocation-metadata.yml
 
 git checkout -b setup-root
 git add ceremony/

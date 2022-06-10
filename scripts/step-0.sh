213475b71456343d6df8b6cd5b5505831888ccd8
Asra Ali <asraa@google.com>
2022-06-10 13:35:21 -0500

    make script fixes
    Signed-off-by: Asra Ali <asraa@google.com>

--- scripts/step-0.sh
+++ scripts/step-0.sh
@@ -3,35 +3,23 @@
 # Print all commands and stop on errors
 set -ex
 
-if [ -z "$GITHUB_USER" ]; then
-    echo "Set GITHUB_USER"
-    exit
-fi
+source "./scripts/utils.sh"
+
+# Check that a github user is set.
+check_user
 
 # Dump the git state
-git status
-git remote -v
+print_git_state
 
 # Setup forks
-git remote rm upstream || true
-git remote add upstream git@github.com:sigstore/root-signing.git
-git remote rm origin || true
-git remote add origin git@github.com:"$GITHUB_USER"/root-signing.git
-git remote -v
-
+setup_forks
 
 # Cleanup branches
-git branch -D setup-root || true
-git branch -D add-key || true
-git branch -D sign-targets || true
-git branch -D sign-snapshot || true
-git branch -D sign-timestamp || true
-git branch -D publish || true
+cleanup_branchs
+clean_state
 
-git clean -d -f
-git checkout main
-git pull upstream main
-git rev-parse HEAD
+# Checkout the working branch
+checkout_branch
 
 # build the tuf binary
 go build -o tuf -tags=pivkey ./cmd/tuf

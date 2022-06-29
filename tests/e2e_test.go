b8ecf4b4591acf65111f522dee889ecf591b402f
asraa <asraa@google.com>
2022-06-28 21:19:13 -0500

    test: Add tests for key rotation and root publishing (#289)
    * test: add a test publishing metadata successfully
    
    Signed-off-by: Asra Ali <asraa@google.com>
    
    * test: add rotate test
    
    Signed-off-by: Asra Ali <asraa@google.com>
    
    Add version test
    
    Signed-off-by: Asra Ali <asraa@google.com>
    
    * update
    
    Signed-off-by: Asra Ali <asraa@google.com>

--- tests/e2e_test.go
+++ tests/e2e_test.go
@@ -7,29 +7,33 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/sigstore/cosign/pkg/cosign"
 	"github.com/sigstore/root-signing/cmd/tuf/app"
 	vapp "github.com/sigstore/root-signing/cmd/verify/app"
+	"github.com/sigstore/root-signing/pkg/keys"
+	prepo "github.com/sigstore/root-signing/pkg/repo"
 
 	"github.com/theupdateframework/go-tuf"
+	"github.com/theupdateframework/go-tuf/client"
 	"github.com/theupdateframework/go-tuf/data"
 )
 
 // TODO(asraa): Add more unit tests, including
-//   * Test >1 threshold signers for root and targets.
 //   * Custom metadata included in targets
 //   * Updating a root version
 //   * Rotating a keyholder
-//   * Root and targets are validated before snapshotting
-//   * Root and target placeholders are removed before snapshotting
 //   * Fetching targets with cosign's API with/without consistent snapshotting
+//   * Rotate a target file
 
 // Create a test HSM key located in a keys/ subdirectory of testDir.
 func createTestHsmKey(testDir string) error {
@@ -72,6 +76,21 @@ func createTestSigner(t *testing.T) string {
 		t.Fatal(err)
 	}
 	return f.Name()
+}
+
+// Verify with the go-tuf client.
+func verifyGoTuf(t *testing.T, repo string, root []byte) (data.TargetFiles, error) {
+	remote, err := vapp.FileRemoteStore(repo)
+	if err != nil {
+		t.Fatal(err)
+	}
+	local := client.MemoryLocalStore()
+	c := client.NewClient(local, remote)
+	if err := c.InitLocal(root); err != nil {
+		t.Fatal(err)
+
+	}
+	return c.Update()
 }
 
 func TestInitCmd(t *testing.T) {
@@ -285,5 +304,283 @@ func TestSnapshotUnvalidatedFails(t *testing.T) {
 	// Snapshot success! We clear the empty placeholder signature in root/targets.
 	if err := app.SnapshotCmd(ctx, td); err != nil {
 		t.Fatalf("expected Snapshot command to pass, got err: %s", err)
+	}
+}
+
+func TestPublishSuccess(t *testing.T) {
+	ctx := context.Background()
+	td := t.TempDir()
+
+	rootCA, rootSigner, err := CreateRootCA()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testTarget := filepath.Join(td, "foo.txt")
+	targetsConfig := map[string]json.RawMessage{testTarget: nil}
+
+	if err := os.WriteFile(testTarget, []byte("abc"), 0600); err != nil {
+		t.Fatal(err)
+	}
+
+	rootSerial, err := CreateTestHsmSigner(td, rootCA, rootSigner)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	snapshotKey := createTestSigner(t)
+	timestampKey := createTestSigner(t)
+
+	// Initialize succeeds.
+	if err := app.InitCmd(ctx, td, "", 1, targetsConfig, snapshotKey, timestampKey); err != nil {
+		t.Fatal(err)
+	}
+
+	// Sign root & targets
+	rootKey, err := GetTestHsmSigner(ctx, td, *rootSerial)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := app.SignCmd(ctx, td, []string{"root", "targets"}, rootKey); err != nil {
+		t.Fatal(err)
+	}
+
+	// Sign snapshot and timestamp
+	if err := app.SnapshotCmd(ctx, td); err != nil {
+		t.Fatalf("expected Snapshot command to pass, got err: %s", err)
+	}
+	snapshotSigner, err := keys.GetSigningKey(ctx, snapshotKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := app.SignCmd(ctx, td, []string{"snapshot"}, snapshotSigner); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := app.TimestampCmd(ctx, td); err != nil {
+		t.Fatalf("expected Timestamp command to pass, got err: %s", err)
+	}
+	timestampSigner, err := keys.GetSigningKey(ctx, timestampKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := app.SignCmd(ctx, td, []string{"timestamp"}, timestampSigner); err != nil {
+		t.Fatal(err)
+	}
+
+	// Successful Publishing!
+	if err := app.PublishCmd(ctx, td); err != nil {
+		t.Fatal(err)
+	}
+
+	// Check versions.
+	store := tuf.FileSystemStore(td, nil)
+	meta, err := store.GetMeta()
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, metaName := range []string{"root.json", "targets.json", "snapshot.json", "timestamp.json"} {
+		md, ok := meta[metaName]
+		if !ok {
+			t.Fatalf("missing %s", metaName)
+		}
+		signed := &data.Signed{}
+		if err := json.Unmarshal(md, signed); err != nil {
+			t.Fatal(err)
+		}
+		sm, err := vapp.PrintAndGetSignedMeta(metaName, signed.Signed)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if sm.Version != 1 {
+			t.Errorf("expected metadata version 1, got %d", sm.Version)
+		}
+	}
+
+	// Verify with go-tuf
+	targetFiles, err := verifyGoTuf(t, td, meta["root.json"])
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(targetFiles) != 1 {
+		t.Fatalf("expected one target, got %d", len(targetFiles))
+	}
+	for name := range targetFiles {
+		if !strings.EqualFold(name, "foo.txt") {
+			t.Fatalf("expected one target foo.txt, got %s", name)
+		}
+	}
+}
+
+func TestRotateRootKey(t *testing.T) {
+	// This tests root key rotation: we use a threshold of 1 with 2 root keys
+	// and expect to rotate one keyholder during an update.
+	ctx := context.Background()
+	td := t.TempDir()
+
+	rootCA, rootSigner, err := CreateRootCA()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testTarget := filepath.Join(td, "foo.txt")
+	targetsConfig := map[string]json.RawMessage{testTarget: nil}
+
+	if err := os.WriteFile(testTarget, []byte("abc"), 0600); err != nil {
+		t.Fatal(err)
+	}
+
+	rootSerial1, err := CreateTestHsmSigner(td, rootCA, rootSigner)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rootSerial2, err := CreateTestHsmSigner(td, rootCA, rootSigner)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	snapshotKey := createTestSigner(t)
+	timestampKey := createTestSigner(t)
+
+	// Initialize succeeds.
+	if err := app.InitCmd(ctx, td, "", 1, targetsConfig, snapshotKey, timestampKey); err != nil {
+		t.Fatal(err)
+	}
+
+	// Sign root & targets with key 1
+	rootKey1, err := GetTestHsmSigner(ctx, td, *rootSerial1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := app.SignCmd(ctx, td, []string{"root", "targets"}, rootKey1); err != nil {
+		t.Fatal(err)
+	}
+
+	// Sign snapshot and timestamp
+	if err := app.SnapshotCmd(ctx, td); err != nil {
+		t.Fatalf("expected Snapshot command to pass, got err: %s", err)
+	}
+	snapshotSigner, err := keys.GetSigningKey(ctx, snapshotKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := app.SignCmd(ctx, td, []string{"snapshot"}, snapshotSigner); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := app.TimestampCmd(ctx, td); err != nil {
+		t.Fatalf("expected Timestamp command to pass, got err: %s", err)
+	}
+	timestampSigner, err := keys.GetSigningKey(ctx, timestampKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := app.SignCmd(ctx, td, []string{"timestamp"}, timestampSigner); err != nil {
+		t.Fatal(err)
+	}
+
+	// Successful Publishing!
+	if err := app.PublishCmd(ctx, td); err != nil {
+		t.Fatal(err)
+	}
+
+	// Check that there are two root key signers: key 1 and key 2.
+	store := tuf.FileSystemStore(td, nil)
+	root, err := prepo.GetRootFromStore(store)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rootRole, ok := root.Roles["root"]
+	if !ok {
+		t.Fatalf("expected root role")
+	}
+	rootKey2, err := GetTestHsmSigner(ctx, td, *rootSerial2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expectedKeyIds := append(rootKey1.Key.IDs(), rootKey2.Key.IDs()...)
+	actualKeyIds := rootRole.KeyIDs
+	sort.Strings(expectedKeyIds)
+	sort.Strings(actualKeyIds)
+	if !cmp.Equal(expectedKeyIds, actualKeyIds) {
+		t.Fatalf("expected key IDs %s, got %s", expectedKeyIds, actualKeyIds)
+	}
+
+	// Now remove the second key and add a third key.
+	if err := os.RemoveAll(filepath.Join(td, "keys", fmt.Sprint(*rootSerial2))); err != nil {
+		t.Fatal(err)
+	}
+	rootSerial3, err := CreateTestHsmSigner(td, rootCA, rootSigner)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a new root.
+	if err := app.InitCmd(ctx, td, td, 1, targetsConfig, snapshotKey, timestampKey); err != nil {
+		t.Fatal(err)
+	}
+
+	// Check the root keys were rotated: expect key 1 and 3.
+	root, err = prepo.GetRootFromStore(store)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rootRole, ok = root.Roles["root"]
+	if !ok {
+		t.Fatalf("expected root role")
+	}
+	rootKey3, err := GetTestHsmSigner(ctx, td, *rootSerial3)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expectedKeyIds = append(rootKey1.Key.IDs(), rootKey3.Key.IDs()...)
+	actualKeyIds = rootRole.KeyIDs
+	sort.Strings(expectedKeyIds)
+	sort.Strings(actualKeyIds)
+	if !cmp.Equal(expectedKeyIds, actualKeyIds) {
+		t.Fatalf("expected key IDs %s, got %s", expectedKeyIds, actualKeyIds)
+	}
+
+	// Expect version 2 for root.
+	if root.Version != 2 {
+		t.Fatalf("expected root version 2, got %d", root.Version)
+	}
+
+	// Sign root & targets
+	rootKey, err := GetTestHsmSigner(ctx, td, *rootSerial1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := app.SignCmd(ctx, td, []string{"root", "targets"}, rootKey); err != nil {
+		t.Fatal(err)
+	}
+
+	// Sign snapshot and timestamp
+	if err := app.SnapshotCmd(ctx, td); err != nil {
+		t.Fatalf("expected Snapshot command to pass, got err: %s", err)
+	}
+	if err := app.SignCmd(ctx, td, []string{"snapshot"}, snapshotSigner); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := app.TimestampCmd(ctx, td); err != nil {
+		t.Fatalf("expected Timestamp command to pass, got err: %s", err)
+	}
+	if err := app.SignCmd(ctx, td, []string{"timestamp"}, timestampSigner); err != nil {
+		t.Fatal(err)
+	}
+
+	// Successful Publishing!
+	if err := app.PublishCmd(ctx, td); err != nil {
+		t.Fatal(err)
+	}
+
+	// Verify with go-tuf
+	meta, err := store.GetMeta()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, err = verifyGoTuf(t, td, meta["root.json"]); err != nil {
+		t.Fatal(err)
 	}
 }

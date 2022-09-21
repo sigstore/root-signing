689353710cc8d74ec6abaaaf54cda3332b1e7d27
Asra Ali <asraa@google.com>
2022-09-21 12:10:26 -0400

    test: use a test repo stack
    Signed-off-by: Asra Ali <asraa@google.com>

--- tests/utils.go
+++ tests/utils.go
@@ -19,6 +19,7 @@
 package test
 
 import (
+	"bytes"
 	"context"
 	"crypto"
 	"crypto/ecdsa"
@@ -27,19 +28,157 @@ import (
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/asn1"
+	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"math/big"
+	"os"
 	"path/filepath"
+	"testing"
 	"time"
 
 	"github.com/go-piv/piv-go/piv"
 	"github.com/sigstore/cosign/cmd/cosign/cli/pivcli"
+	"github.com/sigstore/cosign/pkg/cosign"
+	csignature "github.com/sigstore/cosign/pkg/signature"
 	"github.com/sigstore/root-signing/cmd/tuf/app"
 	"github.com/sigstore/root-signing/pkg/keys"
 	"github.com/sigstore/sigstore/pkg/cryptoutils"
 	"github.com/sigstore/sigstore/pkg/signature"
+	"github.com/theupdateframework/go-tuf"
 )
+
+type repoTestStack struct {
+	ctx           context.Context
+	repoDir       string
+	hsmRootCA     *x509.Certificate
+	hsmRootSigner crypto.PrivateKey
+	targetsConfig map[string]json.RawMessage
+	snapshotRef   string
+	timestampRef  string
+}
+
+// newRepoTestStack initializes a test stack for e2e
+// testing, setting up a root CA for the HSM keys
+func newRepoTestStack(ctx context.Context, t *testing.T) *repoTestStack {
+	td := t.TempDir()
+
+	rootCA, rootSigner, err := CreateRootCA()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return &repoTestStack{
+		repoDir:       td,
+		hsmRootCA:     rootCA,
+		hsmRootSigner: rootSigner,
+		targetsConfig: make(map[string]json.RawMessage, 0),
+		snapshotRef:   createTestSigner(t),
+		timestampRef:  createTestSigner(t),
+	}
+}
+
+func (s *repoTestStack) addTarget(t *testing.T, name, content string, custom json.RawMessage) {
+	testTarget := filepath.Join(s.repoDir, name)
+	if err := os.WriteFile(testTarget, []byte(content), 0600); err != nil {
+		t.Fatal(err)
+	}
+	s.targetsConfig[testTarget] = custom
+}
+
+func (s *repoTestStack) removeTarget(t *testing.T, name string) {
+	delete(s.targetsConfig, filepath.Join(s.repoDir, name))
+}
+
+func (s *repoTestStack) genKey(t *testing.T, hsm bool) string {
+	if hsm {
+		keyRef, err := CreateTestHsmSigner(s.repoDir, s.hsmRootCA, s.hsmRootSigner)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return keyRef
+	}
+	return createTestSigner(t)
+}
+
+func (s *repoTestStack) getSigner(t *testing.T, ref string) signature.Signer {
+	signer, err := signature.LoadSignerFromPEMFile(ref, crypto.SHA256, nil)
+	if err != nil {
+		signer, err := csignature.SignerVerifierFromKeyRef(s.ctx, ref, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return signer
+	}
+	return signer
+}
+
+func (s *repoTestStack) removeHsmKey(t *testing.T, ref string) {
+	if err := os.RemoveAll(filepath.Dir(ref)); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func (s *repoTestStack) getManifest(t *testing.T, manifest string) json.RawMessage {
+	store := tuf.FileSystemStore(s.repoDir, nil)
+	meta, err := store.GetMeta()
+	if err != nil {
+		t.Fatal(err)
+	}
+	md, ok := meta[manifest]
+	if !ok {
+		t.Fatalf("missing %s", manifest)
+	}
+	return md
+}
+
+func (s *repoTestStack) snapshot(t *testing.T, deprecated bool) {
+	if err := app.SnapshotCmd(s.ctx, s.repoDir); err != nil {
+		t.Fatalf("expected Snapshot command to pass, got err: %s", err)
+	}
+	snapshotSigner, err := csignature.SignerVerifierFromKeyRef(s.ctx, s.snapshotRef, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := app.SignCmd(s.ctx, s.repoDir, []string{"snapshot"}, snapshotSigner, deprecated); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func (s *repoTestStack) timestamp(t *testing.T, deprecated bool) {
+	if err := app.TimestampCmd(s.ctx, s.repoDir); err != nil {
+		t.Fatalf("expected timestamp command to pass, got err: %s", err)
+	}
+	timestampSigner, err := csignature.SignerVerifierFromKeyRef(s.ctx, s.timestampRef, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := app.SignCmd(s.ctx, s.repoDir, []string{"timestamp"}, timestampSigner, deprecated); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func (s *repoTestStack) publish(t *testing.T) {
+	if err := app.PublishCmd(s.ctx, s.repoDir); err != nil {
+		t.Fatal(err)
+	}
+}
+
+// Create fake key signer in testDirectory. Returns file reference to signer.
+func createTestSigner(t *testing.T) string {
+	keys, err := cosign.GenerateKeyPair(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	temp := t.TempDir()
+	f, _ := os.CreateTemp(temp, "*.key")
+
+	if _, err := io.Copy(f, bytes.NewBuffer(keys.PrivateBytes)); err != nil {
+		t.Fatal(err)
+	}
+	return f.Name()
+}
 
 func CreateRootCA() (*x509.Certificate, crypto.PrivateKey, error) {
 	// set up our CA certificate
@@ -126,18 +265,10 @@ func createTestAttestations(root *x509.Certificate, rootSigner crypto.PrivateKey
 	return deviceCert, keyCert, nil
 }
 
-func GetTestHsmSigner(ctx context.Context, testDir string, serial uint32) (signature.Signer, error) {
-	// read private key from file.
-	serialStr := fmt.Sprint(serial)
-	privKeyFile := filepath.Join(testDir, "keys", serialStr, serialStr+"_privkey.pem")
-
-	return signature.LoadSignerVerifierFromPEMFile(privKeyFile, crypto.SHA256, nil)
-}
-
-func CreateTestHsmSigner(testDir string, root *x509.Certificate, rootSigner crypto.PrivateKey) (*uint32, error) {
+func CreateTestHsmSigner(testDir string, root *x509.Certificate, rootSigner crypto.PrivateKey) (string, error) {
 	generated, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	pub := &generated.PublicKey
 	n, _ := rand.Int(rand.Reader, big.NewInt(100000000))
@@ -145,16 +276,16 @@ func CreateTestHsmSigner(testDir string, root *x509.Certificate, rootSigner cryp
 
 	deviceCert, keyCert, err := createTestAttestations(root, rootSigner, pub, int(serial))
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	deviceCertPem, err := cryptoutils.MarshalCertificateToPEM(deviceCert)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	keyCertPem, err := cryptoutils.MarshalCertificateToPEM(keyCert)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	keyAndAttestations := &app.KeyAndAttestations{
@@ -172,18 +303,18 @@ func CreateTestHsmSigner(testDir string, root *x509.Certificate, rootSigner cryp
 
 	// Write to repository/keys/SERIAL_NUM/SERIAL_NUM_pubkey.pem, etc
 	if err := app.WriteKeyData(keyAndAttestations, testDir); err != nil {
-		return nil, err
+		return "", err
 	}
 
 	b, err := cryptoutils.MarshalPrivateKeyToPEM(generated)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 
 	serialStr := fmt.Sprint(keyAndAttestations.Attestations.KeyAttestation.Serial)
 	privKeyFile := filepath.Join(testDir, "keys", serialStr, serialStr+"_privkey.pem")
 	if err := ioutil.WriteFile(privKeyFile, []byte(b), 0644); err != nil {
-		return nil, err
+		return "", err
 	}
-	return &serial, nil
+	return privKeyFile, nil
 }

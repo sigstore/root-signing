9b5392550f46ffcd5fc3a059b861412ec9fbfc8b
asraa <asraa@google.com>
2023-05-16 10:15:17 -0500

    ci: add automated issue closing on successful runs (#829)
    * ci: add automated issue closing on successful runs
    
    Signed-off-by: Asra Ali <asraa@google.com>
    
    * fix yamllint
    
    Signed-off-by: Asra Ali <asraa@google.com>
    
    ---------
    
    Signed-off-by: Asra Ali <asraa@google.com>

--- .github/workflows/scripts/e2e-utils.sh
+++ .github/workflows/scripts/e2e-utils.sh
@@ -14,25 +14,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -euo pipefail
-
-source "./.github/workflows/scripts/e2e-utils.sh"
-
 # Gets the name of the currently running workflow file.
 # Note: this requires GITHUB_TOKEN to be set in the workflows.
 this_file() {
     gh api -H "Accept: application/vnd.github.v3+json" "/repos/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" | jq -r '.path' | cut -d '/' -f3
 }
 
-THIS_FILE=$(this_file)
-create_issue_body
+# File is BODY in current directory.
+create_issue_body() {
+    RUN_DATE=$(date --utc)
 
-ISSUE_ID=$(gh -R "$ISSUE_REPOSITORY" issue list --label "bug" --state open -S "$THIS_FILE" --json number | jq '.[0]' | jq -r '.number' | jq 'select (.!=null)')
-
-if [[ -z "$ISSUE_ID" ]]; then
-    # Replace `-`` by ` `, remove the last 4 characters `.yml`. Expected: "snapshot timestamp".
-    TITLE=$(echo "$THIS_FILE" | sed -e 's/\-/ /g' | rev | cut -c5- | rev)
-    GH_TOKEN=$GITHUB_TOKEN gh -R "$ISSUE_REPOSITORY" issue create -t "[bug]: Updating workflow $TITLE" -F ./BODY --label "bug"
-else
-    GH_TOKEN=$GITHUB_TOKEN gh -R "$ISSUE_REPOSITORY" issue comment "$ISSUE_ID" -F ./BODY
-fi
+    # see https://docs.github.com/en/actions/learn-github-actions/environment-variables
+    # https://docs.github.com/en/actions/learn-github-actions/contexts.
+    cat <<EOF >BODY
+Repo: https://github.com/$GITHUB_REPOSITORY/tree/$GITHUB_REF_NAME
+Run: https://github.com/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID
+Workflow file: https://github.com/$GITHUB_REPOSITORY/tree/main/.github/workflows/$THIS_FILE
+Workflow runs: https://github.com/$GITHUB_REPOSITORY/actions/workflows/$THIS_FILE
+Trigger: $GITHUB_EVENT_NAME
+Branch: $GITHUB_REF_NAME
+Date: $RUN_DATE
+EOF
+}
